a0736e49a82194b928646386c5285a4deaa4dc78
YuPo13 <yuliya.povzlo@gmail.com>
2019-11-25 15:07:38 +0200

    Hope I've covered O(n) requirement

--- merge.py
+++ merge.py
@@ -1,6 +1,15 @@
-from typing import List
+"""module docstring"""
+# from typing import List
 
 
-def merge(l1: List[int], l2: List[int]) -> List[int]:
-
-    return []
+def merge(first_list, second_list):
+    """docstring"""
+    unsorted = first_list + second_list
+    sorted_list = []
+    list_length = len(unsorted)
+    i = 0
+    while i < list_length:
+        sorted_list.append(min(unsorted))
+        unsorted.remove(min(unsorted))
+        i += 1
+    return sorted_list
