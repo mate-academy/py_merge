fb12bf2929a28872c5fe04f07216077cb948e42e
Dima <dima.sapsay@gmail.com>
2019-11-25 15:39:15 +0200

    second commit

--- merge.py
+++ merge.py
@@ -8,7 +8,8 @@ from typing import List
 def merge(lst1: List[int], lst2: List[int]) -> List[int]:
     """Func sorted"""
     new_lst = lst1 + lst2
-    for i in range(len(new_lst)):
+    len_lst = len(new_lst)
+    for i in range(len_lst):
         lowest_index = i
         for j in range(i+1, len(new_lst)):
             if new_lst[j] < new_lst[lowest_index]:
