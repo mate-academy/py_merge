146897f7015fa7d049c5eea9f9c42929bc9cf9a4
logotip <logotip123@yahoo.com>
2019-11-25 12:14:30 +0200

    v 1.0

--- merge.py
+++ merge.py
@@ -1,6 +1,31 @@
+"""Two list sort module"""
 from typing import List
 
 
-def merge(l1: List[int], l2: List[int]) -> List[int]:
+def merge(array1: List[int], array2: List[int]) -> List[int]:
+    """
+    Two list sort function
+    :param array1: first list for sort
+    :param array2: second list for sort
+    :return: array1 + array2 sorted list
+    """
+    array1index = 0
+    array2index = 0
+    result = []
 
-    return []
+    while True:
+        if array1index >= len(array1):
+            result.extend(array2[array2index:])
+            break
+        if array2index >= len(array2):
+            result.extend(array1[array1index:])
+            break
+        if array1[array1index] < array2[array2index]:
+            result.append(array1[array1index])
+            print(result)
+            array1index += 1
+        else:
+            result.append(array2[array2index])
+            print(result)
+            array2index += 1
+    return result
