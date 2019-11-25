1ff68bc4a3e07dca9be9cc5792a55a9b4d8507d7
Dima <dima.sapsay@gmail.com>
2019-11-25 15:34:42 +0200

    first commit

--- merge.py
+++ merge.py
@@ -1,6 +1,17 @@
+"""
+New sorted list combining
+"""
+
 from typing import List
 
 
-def merge(l1: List[int], l2: List[int]) -> List[int]:
-
-    return []
+def merge(lst1: List[int], lst2: List[int]) -> List[int]:
+    """Func sorted"""
+    new_lst = lst1 + lst2
+    for i in range(len(new_lst)):
+        lowest_index = i
+        for j in range(i+1, len(new_lst)):
+            if new_lst[j] < new_lst[lowest_index]:
+                lowest_index = j
+        new_lst[i], new_lst[lowest_index] = new_lst[lowest_index], new_lst[i]
+    return new_lst
