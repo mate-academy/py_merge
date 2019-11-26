0853795544afb60a55bf642c7f2767bcc8606a50
Andrii <andrewsotnikov88@gmail.com>
2019-11-26 16:38:05 +0200

    Sorting of two merged list without using any built-in functions

--- merge.py
+++ merge.py
@@ -1,6 +1,17 @@
+"""Sorting of two merged list without using any built-in functions."""
+
 from typing import List
 
 
-def merge(l1: List[int], l2: List[int]) -> List[int]:
+def bubble_sorting(array):
+    """Realization of bubble sort"""
+    for i in range(len(array) - 1):
+        for j in range(len(array) - 1 - i):
+            if array[j] > array[j + 1]:
+                array[j], array[j + 1] = array[j + 1], array[j]
+    return array
 
-    return []
+
+def merge(first_list: List[int], second_list: List[int]) -> List[int]:
+    """Sorting of two lists"""
+    return bubble_sorting(first_list + second_list)
