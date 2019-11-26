7912dca26db03803f081ec8496fa7a833aec3d5e
Platoniuks <platon@alterpost.org>
2019-11-26 08:23:56 +0200

    resolved

--- merge.py
+++ merge.py
@@ -1,6 +1,13 @@
+"""Classic bubble sorting #2"""
 from typing import List
 
 
-def merge(l1: List[int], l2: List[int]) -> List[int]:
-
-    return []
+def merge(arr1: List[int], arr2: List[int]) -> List[int]:
+    """General func"""
+    new_arr = arr1 + arr2
+    arr_length = len(new_arr) - 1
+    for i in range(arr_length):
+        for j in range(arr_length - i):
+            if new_arr[j] > new_arr[j + 1]:
+                new_arr[j], new_arr[j + 1] = new_arr[j + 1], new_arr[j]
+    return new_arr
