6b5a0d6c1ae6d3def116111876ba6722e5d5d849
Platoniuks <platon@alterpost.org>
2019-11-27 18:28:34 +0200

    third try

--- merge.py
+++ merge.py
@@ -1,18 +1,18 @@
 """Make a new sorted list combining two sorted lists"""
 from typing import List
 
-
 def merge(arr1: List[int], arr2: List[int]) -> List[int]:
     """General func"""
-    new_arr = arr1 + arr2
-    return quick_sort(new_arr)
-
-
-def quick_sort(arr):
-    """Quick sorting function"""
-    if len(arr) < 2:
-        return arr
-    support = arr[0]
-    less = [i for i in arr[1:] if i <= support]
-    greater = [i for i in arr[1:] if i > support]
-    return quick_sort(less) + [support] + quick_sort(greater)
+    length_arr1 = len(arr1)
+    length_arr2 = len(arr2)
+    new_arr = []
+    i, j = 0, 0
+    while i < length_arr1 and j < length_arr2:
+        if arr1[i] < arr2[j]:
+            new_arr.append(arr1[i])
+            i += 1
+        else:
+            new_arr.append(arr2[j])
+            j += 1
+    new_arr = new_arr + arr1[i:] + arr2[j:]
+    return new_arr
