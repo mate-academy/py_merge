9724c48e4e54cec80873f15bd4341395cdd6cca4
zaikovlad95@gmail.com <Makepeacenotwar1995>
2019-12-11 23:41:05 +0200

    Merge two lists and return new sorted list.

--- merge.py
+++ merge.py
@@ -1,6 +1,19 @@
+"""Merge module."""
 from typing import List
 
 
-def merge(l1: List[int], l2: List[int]) -> List[int]:
+def merge(first_list: List[int], second_list: List[int]) -> List[int]:
+    """Merge two lists into one and return it sorted."""
+    fst_lst_length = len(first_list)
+    snd_lst_length = len(second_list)
+    new_list = []
 
-    return []
+    i, j = 0, 0
+    while i < fst_lst_length and j < snd_lst_length:
+        if first_list[i] < second_list[j]:
+            new_list.append(first_list[i])
+            i += 1
+        else:
+            new_list.append(second_list[j])
+            j += 1
+    return new_list + first_list[i:] + second_list[j:]
